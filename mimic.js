355c3e0f69e8f2a32e8ac9ff154635b816036417
ak <ak@3dlam.com>
2023-12-11 09:46:35 +0300

    fix animation play

--- mimic.js
+++ mimic.js
@@ -3,7 +3,6 @@
  * 
  *************************************************************/
 
-
 /**
  * Clear left-eye, right-eye and mouth.
  */
@@ -141,6 +140,13 @@ var right_eye_timeline;
 var mouth_timeline;
 
 
+function check_easing(easing){
+    if (easing && easing !='string'){
+        return true
+    }
+    return false
+}
+
 /**
  * Converts data from server format to anime.js format.
  * @param {*} data 
@@ -163,23 +169,26 @@ function to_anime_data_format(data){
                 translateY: mimic_item[`y_${obj}`],
                 duration: 300,
                 endDelay: mimic_item['delay'],
-                easing: mimic_item['easing'] ? mimic_item[easing] : 'linear',
-                // Callback runs before movement begins.
+                easing: check_easing(mimic_item['easing']) ? mimic_item['easing'] : 'linear',
+                // Callback runs before mimic tramslation begins.
                 // Changes style of mimic.
                 begin: function(a, m=mimic_item, elem=obj) {
                     let id = obj.replace(/_/g, '-')
                     let w = m[`w_${elem}`]
                     let h = m[`h_${elem}`]
                     let style = m[`style_${elem}`]
-
+                    console.log('start callback')
                     switch(style) {
                         case 0: // Arc
+                            console.log('arc')
                             draw_arc(id, w, h)
                             break
                         case 1: //Line
+                            console.log('line')
                             draw_line(id, w, h)
                             break
                         case 2: //Inverted arc
+                            console.log('inverted arc')
                             draw_arc(id, w, h, invert=true)
                             break
                     }
@@ -190,7 +199,7 @@ function to_anime_data_format(data){
         }
         result.push(frame)
     }
-    console.log(result)
+    console.log('anime js result', result)
     return result
 }
 
@@ -204,114 +213,114 @@ function to_anime_data_format(data){
 function run_mimic(data){
     anime_data = to_anime_data_format(data)
 
-    left_eye_timeline.remove('#left-eye')
-    right_eye_timeline.remove('#right-eye')
-    mouth_timeline.remove('#mouth')
+    left_eye_timeline.reset()
+    right_eye_timeline.reset()
+    mouth_timeline.reset()
+    left_eye_timeline = anime.timeline();
+    right_eye_timeline = anime.timeline();
+    mouth_timeline = anime.timeline();
 
     for (let obj of anime_data) {
         left_eye_timeline.add(obj[0])
         right_eye_timeline.add(obj[1])
         mouth_timeline.add(obj[2])
     }
-    left_eye_timeline.restart()
-    right_eye_timeline.restart()
-    mouth_timeline.restart()
 
 }
 
-function test_data(){
-    json_data = `{ 
-        "mimic_items": [
-            {
-                "style_left_eye": 2,
-                "x_left_eye" : 100,
-                "y_left_eye": 0,
-                "w_left_eye" : 300,
-                "h_left_eye" : 40,
+// function test_data(){
+//     json_data = `{ 
+//         "mimic_items": [
+//             {
+//                 "style_left_eye": 2,
+//                 "x_left_eye" : 100,
+//                 "y_left_eye": 0,
+//                 "w_left_eye" : 300,
+//                 "h_left_eye" : 40,
             
-                "style_right_eye": 2,
-                "x_right_eye" : 100,
-                "y_right_eye" : 0,
-                "w_right_eye" : 300,
-                "h_right_eye" : 40,
+//                 "style_right_eye": 2,
+//                 "x_right_eye" : 100,
+//                 "y_right_eye" : 0,
+//                 "w_right_eye" : 300,
+//                 "h_right_eye" : 40,
             
-                "style_mouth": 1,
-                "x_mouth" : 0,
-                "y_mouth" : 50,
-                "w_mouth" : 300,
-                "h_mouth" : 40,
+//                 "style_mouth": 1,
+//                 "x_mouth" : 0,
+//                 "y_mouth" : 50,
+//                 "w_mouth" : 300,
+//                 "h_mouth" : 40,
             
-                "delay" : 500,
-                "easing": "",
-                "order": 0 
+//                 "delay" : 500,
+//                 "easing": "",
+//                 "order": 0 
 
-            },
+//             },
 
-            {
-                "style_left_eye": 2,
-                "x_left_eye" : 0,
-                "y_left_eye": 0,
-                "w_left_eye" : 300,
-                "h_left_eye" : 40,
+//             {
+//                 "style_left_eye": 2,
+//                 "x_left_eye" : 0,
+//                 "y_left_eye": 0,
+//                 "w_left_eye" : 300,
+//                 "h_left_eye" : 40,
             
-                "style_right_eye": 2,
-                "x_right_eye" : 0,
-                "y_right_eye" : 0,
-                "w_right_eye" : 300,
-                "h_right_eye" : 40,
+//                 "style_right_eye": 2,
+//                 "x_right_eye" : 0,
+//                 "y_right_eye" : 0,
+//                 "w_right_eye" : 300,
+//                 "h_right_eye" : 40,
             
-                "style_mouth": 1,
-                "x_mouth" : 0,
-                "y_mouth" : 0,
-                "w_mouth" : 300,
-                "h_mouth" : 40,
+//                 "style_mouth": 1,
+//                 "x_mouth" : 0,
+//                 "y_mouth" : 0,
+//                 "w_mouth" : 300,
+//                 "h_mouth" : 40,
             
-                "delay" : 1000,
-                "easing": "",
-                "order": 1 
+//                 "delay" : 1000,
+//                 "easing": "",
+//                 "order": 1 
 
-            },
+//             },
 
-            {
-                "style_left_eye": 0,
-                "x_left_eye" : 0,
-                "y_left_eye": 0,
-                "w_left_eye" : 300,
-                "h_left_eye" : 40,
+//             {
+//                 "style_left_eye": 0,
+//                 "x_left_eye" : 0,
+//                 "y_left_eye": 0,
+//                 "w_left_eye" : 300,
+//                 "h_left_eye" : 40,
             
-                "style_right_eye": 0,
-                "x_right_eye" : 0,
-                "y_right_eye" : 0,
-                "w_right_eye" : 300,
-                "h_right_eye" : 40,
+//                 "style_right_eye": 0,
+//                 "x_right_eye" : 0,
+//                 "y_right_eye" : 0,
+//                 "w_right_eye" : 300,
+//                 "h_right_eye" : 40,
             
-                "style_mouth": 2,
-                "x_mouth" : 0,
-                "y_mouth" : 0,
-                "w_mouth" : 300,
-                "h_mouth" : 40,
+//                 "style_mouth": 2,
+//                 "x_mouth" : 0,
+//                 "y_mouth" : 0,
+//                 "w_mouth" : 300,
+//                 "h_mouth" : 40,
             
-                "delay" : 5,
-                "easing": "",
-                "order": 2 
+//                 "delay" : 5,
+//                 "easing": "",
+//                 "order": 2 
 
-            }
-        ]
-    }`;
+//             }
+//         ]
+//     }`;
 
-    data = JSON.parse(json_data)['mimic_items'] // Array of mimic_items
-    run_mimic(data)
-}
+//     data = JSON.parse(json_data)['mimic_items'] // Array of mimic_items
+//     run_mimic(data)
+// }
 
-function test(){
-    test_data();
-    var delay = 1000; //1 second
+// function test(){
+//     test_data();
+//     var delay = 1000; //1 second
 
-    setTimeout(function() {
-        console.log('aborted')
-        test_data()
-    }, delay);
-}
+//     // setTimeout(function() {
+//     //     console.log('aborted')
+//     //     test_data()
+//     // }, delay);
+// }
 
 
 /*************************************************************
@@ -322,6 +331,7 @@ let socket = new WebSocket(`ws://127.0.0.1:8001/websocket`);
 
 socket.onmessage = function(event) {
     data = JSON.parse(event.data)['mimic_items'] // Array of mimic_items
+    console.log('from server', data)
     run_mimic(data)
 };
 
--- mimic_server.py
+++ mimic_server.py
@@ -14,6 +14,26 @@ logging.basicConfig(filename='mimic-server.log',
                      level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler())
 
+
+
+class WebsocketRegistry:
+    def __init__(self):
+        self.active_websockets = []
+
+    def add_listener(self, listener):
+        self.active_websockets.append(listener)
+
+    def remove_listener(self, listener):
+        self.active_websockets.remove(listener)
+
+    def send_messages(self, msg_txt):
+        print(self.active_websockets)
+        for ws in self.active_websockets:
+            ws.write_message(msg_txt)
+
+registry = WebsocketRegistry()
+
+
 class Web():
 
     class HTTPHandler(tornado.web.RequestHandler):
@@ -21,10 +41,12 @@ class Web():
             self.write("Hello, I'am mimic app")
 
         def post(self):
-            #data = self.get_argument('data', 'No data received')
-            pass
+            data = json.loads(self.request.body)
+            print('data', data)
+            global registry
+            registry.send_messages(data)
+            return
 
-        
     class WebSocketHandler(tornado.websocket.WebSocketHandler):
             
             def check_origin(self, origin):
@@ -32,16 +54,17 @@ class Web():
             
             def open(self):
                 info("Mimic websocket opened")
-                pass
+                global registry
+                registry.add_listener(self)
 
             def on_close(self):
                 info("Mimic webcoket closed")
+                global registry
+                registry.remove_listener(self)
 
             def on_message(self, m):
                 pass
 
-            def msg(self, data):
-                self.write_message(json.dumps({"data": data}))
                 
     def __init__(self):
         settings = {"static_path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "inc")}
